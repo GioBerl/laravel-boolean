242d7dc3ba9ff22f7799cd6d837217600a15012c
Gio <giovanni.berl1@gmail.com>
2020-07-01 11:35:36 +0200

    jumbo

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,5 +3,34 @@
 @section('page-title', 'homepage')
 
 @section('content')
-<h1>Benvenuto</h1>
+
+<main>
+    <div class="container">
+        <section class="jumbo">
+            <div class="jumbo-left">
+                <h1 class="jumbo-title">
+                    Diventa <strong>sviluppatore web</strong>
+                </h1>
+                <p>
+                    Trasformiamo la tua passione in una carriera. Se non trovi lavoro, ti rimborsiamo.
+                </p>
+                <ul class="">
+                    <li>
+                        <strong>6 mesi</strong> di corso intensivo online in diretta
+                    </li>
+                    <li>
+                        <strong>Nessuna competenza</strong> di programmazione richiesta
+                    </li>
+                    <li>
+                        Siamo certi del tuo successo, altrimenti <strong>ti rimborsiamo</strong>
+                    </li>
+                </ul>
+            </div>
+            <div class="jumbo-right">
+                <img src="{{ asset('img/jumbo/pc-black-gif.gif') }}" alt="jumbo image">
+            </div>
+        </section>
+    </div>
+</main>
+
 @endsection
--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,23 @@
 <footer>
-    qui va il footer
+    <div class="container">
+        <div class="footer-top">
+            <div class="footer-top-left">
+                <img src="{{ asset('img/logo-white.png')}}" alt="logo image">
+                <div class="info">
+                    <p>Sede Operativa</p>
+                    <p>Via Carducci 12 - 20123 Milano</p>
+                    <p>Tel: 02-40031288</p>
+                </div>
+            </div>
+            <div class="footer-top-right">
+                <ul>
+                    <li><a href="#">Lezione Gratuita</a></li>
+                    <li><a href="#">Domande Frequenti</a></li>
+                    <li><a href="#">Privacy Policy</a></li>
+                    <li><a href="#">Lavora con noi</a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="footer-bottom">footer bottom</div>
+    </div>
 </footer>
